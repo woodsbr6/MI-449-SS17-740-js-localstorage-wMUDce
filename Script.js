08b02e1f74ba2d3f5e43918fa7fba5191cababfd
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-02 23:22:44 -0400

    black to white

---
+++ Script.js
@@ -0,0 +1,11 @@
+function toggleNightMode() {
+  var color = document.getElementById('nightLight').style.color
+  var backgroundColor = document.getElementById('nightLight').style.backgroundColor
+  if (color == "black" && backgroundColor == "white") {
+    document.getElementById('nightLight').style.color="white"
+    document.getElementById('nightLight').style.backgroundColor="black"
+  } else {
+    document.getElementById('nightLight').style.color="black"
+    document.getElementById('nightLight').style.backgroundColor="white"
+  }
+}
