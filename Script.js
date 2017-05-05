66fac280f9e66ddfe7b76a2990a1ebcf6b631f1e
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-05 12:00:39 -0400

    localstorage

--- Script.js
+++ Script.js
@@ -1,11 +1,13 @@
-function toggleNightMode() {
+function toggleNightMode () {
   var color = document.getElementById('nightLight').style.color
   var backgroundColor = document.getElementById('nightLight').style.backgroundColor
-  if (color == "black" && backgroundColor == "white") {
-    document.getElementById('nightLight').style.color="white"
-    document.getElementById('nightLight').style.backgroundColor="black"
+  if (color === 'black' && backgroundColor === 'white') {
+    document.getElementById('nightLight').style.color = 'white'
+    document.getElementById('nightLight').style.backgroundColor = 'black'
   } else {
-    document.getElementById('nightLight').style.color="black"
-    document.getElementById('nightLight').style.backgroundColor="white"
+    document.getElementById('nightLight').style.color = 'black'
+    document.getElementById('nightLight').style.backgroundColor = 'white'
   }
 }
+
+var nightLight = window.localStorage.getItem('nightLight') === 'true'
