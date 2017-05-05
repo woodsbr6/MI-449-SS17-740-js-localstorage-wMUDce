ecdacd6458095c1f7c4862f1b6e3b5c64cff9e30
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-05 16:42:28 -0400

    updates

--- Script.js
+++ Script.js
@@ -1,19 +1,27 @@
 function toggleNightMode () {
-  var color = document.getElementById('nightLight').style.color
-  var backgroundColor = document.getElementById('nightLight').style.backgroundColor
-  if (color === 'black' && backgroundColor === 'white') {
-    document.getElementById('nightLight').style.color = 'white'
-    document.getElementById('nightLight').style.backgroundColor = 'black'
+  if (nightLight.className == 'daymode') {
+      nightLight.className = 'nightmode'
   } else {
-    document.getElementById('nightLight').style.color = 'black'
-    document.getElementById('nightLight').style.backgroundColor = 'white'
+    nightLight.className = 'daymode'
   }
-var nightLight = window.localStorage.getItem('nightLight')
-if (nightLight === null) {
-  nightLight = window.prompt('nightLight')
-  if (nightLight === null) {
-    nightLight = 'nightLight'
-  } else {
-    window.localStorage.setItem('toggleNightMode')
-  }
-} }
+}
+var button = document.getElementById('themeBut')
+button.addEventListener('click', toggleNightMode)
+//   var nightLight = window.localStorage.getItem('nightLight')
+//   if (nightLight === null) {
+//     nightLight = window.prompt('nightLight')
+//   } else {
+//     window.localStorage.setItem('toggleNightMode')
+//   }
+// }
+
+//     if(typeof(Storage) !== "undefined") {
+//         if (localStorage.toggleNightMode) {
+//             localStorage.toggleNightMode = Number(localStorage.toggleNightMode)+1
+//         } else {
+//             localStorage.toggleNightMode = 1
+//         }
+//         document.getElementById("nightLight").innerHTML = "You have clicked the button " + localStorage.toggleNightMode + " time(s)."
+//     } else {
+//         document.getElementById("nightLight").innerHTML = "#"
+// } }
