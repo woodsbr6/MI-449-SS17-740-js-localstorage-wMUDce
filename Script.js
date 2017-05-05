352c4d7376e7d647f5aca2c70fadb80983f8e926
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-05 17:06:54 -0400

    updates

--- Script.js
+++ Script.js
@@ -17,21 +17,3 @@ if (bodyClass !== null) {
 
 var button = document.getElementById('themeBut')
 button.addEventListener('click', toggleNightMode)
-
-//   if (nightLight === null) {
-//     nightLight = window.prompt('nightLight')
-//   } else {
-//     window.localStorage.setItem('toggleNightMode')
-//   }
-// }
-
-//     if(typeof(Storage) !== "undefined") {
-//         if (localStorage.toggleNightMode) {
-//             localStorage.toggleNightMode = Number(localStorage.toggleNightMode)+1
-//         } else {
-//             localStorage.toggleNightMode = 1
-//         }
-//         document.getElementById("nightLight").innerHTML = "You have clicked the button " + localStorage.toggleNightMode + " time(s)."
-//     } else {
-//         document.getElementById("nightLight").innerHTML = "#"
-// } }
