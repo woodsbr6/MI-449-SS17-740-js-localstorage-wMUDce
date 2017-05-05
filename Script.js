dba439dd8a608fb10f94d1e45ecc75f76ca82764
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-05-05 17:05:50 -0400

    updates

--- Script.js
+++ Script.js
@@ -1,13 +1,23 @@
 function toggleNightMode () {
-  if (nightLight.className == 'daymode') {
-      nightLight.className = 'nightmode'
+  if (nightLight.className === 'daymode') {
+    nightLight.className = 'nightmode'
   } else {
     nightLight.className = 'daymode'
   }
+  window.localStorage.setItem('bodyClass', nightLight.className)
 }
+
+var nightLight = document.getElementById('nightLight')
+var bodyClass = window.localStorage.getItem('bodyClass')
+if (bodyClass !== null) {
+  nightLight.className = bodyClass
+} else {
+  nightLight.className = 'daymode'
+}
+
 var button = document.getElementById('themeBut')
 button.addEventListener('click', toggleNightMode)
-//   var nightLight = window.localStorage.getItem('nightLight')
+
 //   if (nightLight === null) {
 //     nightLight = window.prompt('nightLight')
 //   } else {
